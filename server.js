1bbee4e8d65f6e8e9f5719252a277718f9bb6ca9
Rosebud <rkle408@gmail.com>
2022-09-29 19:44:47 -0700

    added uuid npm, added more skeleton to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
      console.log(`Express server listening on http://localhost:${PORT}`)
